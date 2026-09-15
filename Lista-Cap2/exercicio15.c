54ba1b9ef3155dac7b637c8c0e1de9db7fda0065
João Tezolini Sugahara <142631307+Joao-Tezolini@users.noreply.github.com>
2026-09-15 19:09:54 +0000

    exercicios 7 a 15 feitos

--- Lista-Cap2/exercicio15.c
+++ Lista-Cap2/exercicio15.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    
+    float nota1, nota2, nota3, nota4;
+
+    printf("Insira as notas 1, 2, 3 e 4 com espacos entre elas: ");
+    scanf("%f %f %f %f", &nota1, &nota2, &nota3, &nota4);
+
+    float media_simples = (nota1 + nota2 + nota3 +nota4)/4;
+    float media_pond = (nota1 + nota2 + nota3*2 + nota4*2)/6;
+
+    printf("A media simples das notas: %.2f, %.2f, %.2f, %.2f eh: %2.f\nE a media ponderada eh: %.2f\n\n", nota1, nota2, nota3, nota4, media_simples, media_pond);
+
+    return 0;
 }
---
+++ Lista-Cap2/exercicio16.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float degrau_altura; // cm
+    float altura_total_escada; // m
+
+    int num_degraus = altura_total_escada/degrau_altura;
+
+    
+
+}
